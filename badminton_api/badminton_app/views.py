fadadf8f32c2dd13d22001c82fd6a9a165fdabbe
tt14 <11046014@ntub.edu.tw>
2024-05-15 14:02:44 +0800

    -刪除 api

--- badminton_api/badminton_app/views.py
+++
@@ -1,8 +0,0 @@
-# badminton_app/views.py
-from rest_framework import viewsets
-from .models import MyModel  # 检查导入语句是否正确
-from .serializers import MyModelSerializer  # 确保序列化器导入没有错误
-
-class MyModelViewSet(viewsets.ModelViewSet):
-    queryset = MyModel.objects.all()
-    serializer_class = MyModelSerializer
