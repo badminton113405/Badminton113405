721100bab81b26128ea081e385570b999153cd21
xuan <11046017@ntub.edu.tw>
2024-06-03 05:00:28 +0800

    -修改 註冊表單內容

--- shuttle/src/views/VIP/RegisterView.vue
+++ shuttle/src/views/VIP/RegisterView.vue
@@ -8,17 +8,54 @@
       </div>
 
       <div class="input-group">
+        <label for="gender">性別 *</label>
+        <select v-model="form.gender" id="gender" required>
+          <option value="" disabled>請選擇</option>
+          <option value="male">男</option>
+          <option value="female">女</option>
+          <option value="other">其他</option>
+        </select>
+      </div>
+
+      <div class="input-group">
+        <label for="dob">生日 *</label>
+        <div class="dob-container">
+          <input v-model="form.year" type="number" id="year" placeholder="年" required class="dob">
+          <input v-model="form.month" type="number" id="month" placeholder="月" required class="dob">
+          <input v-model="form.day" type="number" id="day" placeholder="日" required class="dob">
+        </div>
+      </div>
+
+      <div class="input-group">
+        <label for="occupation">職業 *</label>
+        <input v-model="form.occupation" type="text" id="occupation" required>
+      </div>
+
+      <div class="input-group">
+        <label for="phone">電話 *</label>
+        <input v-model="form.phone" type="text" id="phone" required>
+      </div>
+
+      <div class="input-group">
+        <label for="email">電子郵件 *</label>
+        <input v-model="form.email" type="email" id="email" required>
+      </div>
+
+      <div class="input-group">
         <label for="username">設置帳號 *</label>
         <input v-model="form.username" type="text" id="username" required minlength="8">
       </div>
+
       <div class="input-group">
         <label for="password">設置密碼 *</label>
         <input v-model="form.password" type="password" id="password" required minlength="8">
       </div>
+
       <div class="input-group">
         <label for="confirmPassword">確認密碼 *</label>
         <input v-model="form.confirmPassword" type="password" id="confirmPassword" required minlength="8">
       </div>
+
       <div class="button-group">
         <button type="button" class="cancel" @click="resetForm">取消</button>
         <button type="submit" class="confirm">確認</button>
@@ -49,23 +86,29 @@ export default {
     };
   },
   methods: {
-    submitForm() {
-      alert("test");
+    async submitForm() {
       if (this.form.password !== this.form.confirmPassword) {
         alert('密碼不一致');
         return;
       }
-      axios.post('http://127.0.0.1:8000/api/register/', {
-        username: this.form.username,
-        password: this.form.password,
-        email: this.form.email
-      })
-        .then(response => {
-          alert(response.data.message);
-        })
-        .catch(error => {
-          alert(error.response.data.message);
+      
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/register/', {
+          name: this.form.name,
+          gender: this.form.gender,
+          dob: `${this.form.year}-${this.form.month}-${this.form.day}`,
+          occupation: this.form.occupation,
+          phone: this.form.phone,
+          email: this.form.email,
+          username: this.form.username,
+          password: this.form.password
         });
+
+        alert(response.data.message);
+        this.$router.push({ name: 'Login' });
+      } catch (error) {
+        alert(error.response.data.message);
+      }
     },
     resetForm() {
       this.$router.push({ name: 'Login' }).catch(err => {
@@ -74,31 +117,21 @@ export default {
         }
       });
     }
-
   }
 };
 </script>
 
 <style scoped>
 @media screen and (max-width: 320px) {
-
-  /* iPhone SE 的宽度 */
-  /* 适应较小屏幕的调整 */
   .g-header-container.fixed {
     padding: 5px;
-    /* 减少填充 */
   }
-
   .g-footer-container {
     padding: 5px;
-    /* 减少填充 */
   }
-
   .m-button {
     margin-right: 5px;
-    /* 减少边距 */
     font-size: 12px;
-    /* 减少字体大小 */
   }
 }
 
@@ -106,15 +139,10 @@ export default {
   font-family: "Zen Kurenaido", sans-serif;
   margin-top: 30px;
   margin-left: 10px;
-  /* Add left margin */
   margin-right: 10px;
-  /* Add right margin */
   display: flex;
-  /* Center content */
   justify-content: center;
-  /* Center content */
   flex-direction: column;
-  /* Arrange content vertically */
   text-align: center;
   background-color: #f9f9f9;
 }
