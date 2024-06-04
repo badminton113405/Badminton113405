4d5618123329b9358a2b15aa2cb857b1e4920693
Smile <11046017@ntub.edu.tw>
2024-06-04 20:16:08 +0800

    -測試 123

--- shuttle/src/views/VIP/VIPView.vue
+++ shuttle/src/views/VIP/VIPView.vue
@@ -38,7 +38,6 @@
                 <input type="password" id="password" name="password">
             </div>
         </div>
-        <!--123-->
         <div class="course-records">
             <div>課程紀錄:</div>
         </div>
