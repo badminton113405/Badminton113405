78c361b4a606320760a6e63af8bf9c24b0d88fd5
0908Yuan <161016725+0908Yuan@users.noreply.github.com>
2024-06-26 21:58:28 +0800

    -新增 細項

--- shuttle/src/views/VIP/VIPView.vue
+++ shuttle/src/views/VIP/VIPView.vue
@@ -1,55 +1,88 @@
 <template>
   <div class="VIP">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <body>
-    <div class="container">
+      <div class="container">
         <div class="title">會員中心</div>
         <div class="button">
-            <button class="b-button">編輯個人資料</button>
-            <router-link to="/login"><button class="l-button">登出</button></router-link>
+          <button class="b-button">編輯個人資料</button>
+          <router-link to="/login"><button class="l-button">登出</button></router-link>
         </div>
         <div class="profile-info">
           <div>
-                <label for="name">姓名:</label>
-                <input type="text" id="name" name="name">
-            </div>
-            <div>
-                <label for="title">稱謂:</label>
-                <input type="text" id="title" name="title">
-            </div>
-            <div>
-                <label for="nickname">暱稱:</label>
-                <input type="text" id="nickname" name="nickname">
-            </div>
-            <div>
-                <label for="dob">出生年月日:</label>
-                <input type="date" id="dob" name="dob">
-            </div>
-            <div>
-                <label for="phone">聯絡電話:</label>
-                <input type="text" id="phone" name="phone">
-            </div>
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email">
-            </div>
-            <div>
-                <label for="password">密碼:</label>
-                <input type="password" id="password" name="password">
-            </div>
+            <label for="name">姓名:</label>
+            <input type="text" id="name" name="name" v-model="userInfo.username" readonly>
+          </div>
+          <div>
+            <label for="title">稱謂:</label>
+            <input type="text" id="title" name="title">
+          </div>
+          <div>
+            <label for="nickname">暱稱:</label>
+            <input type="text" id="nickname" name="nickname"  v-model="userInfo.nickname" readonly>
+          </div>
+          <div>
+            <label for="dob">出生年月日:</label>
+            <input type="date" id="dob" name="dob">
+          </div>
+          <div>
+            <label for="phone">聯絡電話:</label>
+            <input type="text" id="phone" name="phone">
+          </div>
+          <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email">
+          </div>
+          <div>
+            <label for="password">密碼:</label>
+            <input type="password" id="password" name="password">
+          </div>
         </div>
         <div class="course-records">
-            <div>課程紀錄:</div>
+          <div>課程紀錄:</div>
         </div>
-    </div>
-</body>
+      </div>
+    </body>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      userInfo: {
+        username: '',
+        full_name: '',
+        gender: '',
+        birth_date: '',
+        nickname: '',
+        phone: '',
+        email: '',
+      }
+    };
+  },
+  created() {
+    this.fetchUserInfo();  // Fetch user info when component is created
+  },
+  methods: {
+    async fetchUserInfo() {
+      try {
+        const response = await axios.get('/api/profile');  // Fetch user info from backend
+        this.userInfo = response.data;  // Update userInfo with data from the backend
+      } catch (error) {
+        console.error('Failed to fetch user info:', error);
+      }
+    }
+  }
 }
 </script>
+
+
+
+
 
 <style scoped>
 @media screen and (max-width: 320px) {
@@ -67,9 +100,9 @@ export default {
 
   .m-button {
     margin-right: 5px;
-   
+
     font-size: 12px;
-   
+
   }
 }
 
@@ -101,13 +134,15 @@ export default {
   margin-bottom: 20px;
 }
 
-.profile-info, .course-records {
+.profile-info,
+.course-records {
   text-align: left;
   margin-bottom: 20px;
 }
 
-.profile-info div, .course-records div {
-  margin-bottom: 10px; 
+.profile-info div,
+.course-records div {
+  margin-bottom: 10px;
 }
 
 .profile-info label {
