fc5240967b734b77dc14d46c46cfb341420f4415
0908Yuan <161016725+0908Yuan@users.noreply.github.com>
2024-07-18 01:52:08 +0800

    -建立社群空間

--- BADMINTON/app/views.py
+++ BADMINTON/app/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def community(request):
+    comments = [
+        {'text': '一起打球嗎', 'editing': False},
+        {'text': '有人要一起上團課嗎', 'editing': False}
+    ]
+    return render(request, 'community.html', {'comments': comments})
