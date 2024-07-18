b58084959fd014f24ac317e060833c5199530c79
0908Yuan <161016725+0908Yuan@users.noreply.github.com>
2024-07-19 01:32:34 +0800

    -新增教練團隊

--- BADMINTON/app/urls.py
+++ BADMINTON/app/urls.py
@@ -5,4 +5,12 @@ from . import views
 urlpatterns = [
     path('', home, name='home'),
     path('community-view/', views.community, name='community'),
+    path('beginner-view/', views.beginner, name='beginner'),
+    path('competitive-view/', views.competitive, name='competitive'),
+    path('onetoone-view/', views.onetoone, name='onetoone'),
+    path('zerodozen-view/', views.zerodozen, name='zerodozen'),
+    path('ZZT/', views.ZZT, name='ZZT'),
+    path('CYZ/', views.CYZ, name='CYZ'),
+    path('ZBY/', views.ZBY, name='ZBY'),
+    path('HWH/', views.HWH, name='HWH'),
 ]
--- BADMINTON/app/views.py
+++ BADMINTON/app/views.py
@@ -9,3 +9,27 @@ def community(request):
         {'text': '有人要一起上團課嗎', 'editing': False}
     ]
     return render(request, 'community.html', {'comments': comments})
+
+def beginner(request):
+    return render(request, 'beginner.html')
+
+def competitive(request):
+    return render(request, 'competitive.html')
+
+def onetoone(request):
+    return render(request, 'onetoone.html')
+
+def zerodozen(request):
+    return render(request, 'zerodozen.html')
+
+def ZZT(request):
+    return render(request, 'ZZT.html')
+
+def HWH(request):
+    return render(request, 'HWH.html')
+
+def CYZ(request):
+    return render(request, 'CYZ.html')
+
+def ZBY(request):
+    return render(request, 'ZBY.html')
