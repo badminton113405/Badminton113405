c51633b6063245ca5c7f8499a6520f8961ecaad8
0908Yuan <161016725+0908Yuan@users.noreply.github.com>
2024-10-13 17:00:39 +0800

    -修改 資料庫

--- BADMINTON/BADMINTON/settings.py
+++ BADMINTON/BADMINTON/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'BADMINTON.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -100,7 +100,8 @@ DATABASES = {
         },
     }
 }
-'''
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
